3c9f48e5b61db617af1efc87a85bcba78a2d72c8
allymf <alyssonfacanhamoreira@gmail.com>
2020-01-12 22:52:36 -0300

    Create a basic viewModel for the favorite screen

---
+++ ConcreteRecruiting/ConcreteRecruiting/Scenes/Favorites/FavoritesViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  FavoritesViewModel.swift
+//  ConcreteRecruiting
+//
+//  Created by Alysson Moreira on 12/01/20.
+//  Copyright © 2020 Alysson Moreira. All rights reserved.
+//
+
+import Foundation
+
+class FavoritesViewModel {
+    
+    private let favoriteManager: FavoriteMoviesManager
+    
+    private var favorites: [Int] {
+        didSet {
+            self.acquireMovies(from: self.favorites)
+        }
+    }
+    
+    private var movieViewModels = [MovieCellViewModel]()
+    
+    init(favoriteManager: FavoriteMoviesManager) {
+        self.favoriteManager = favoriteManager
+        self.favorites = self.favoriteManager.getAllFavorites()
+    }
+    
+    // TODO: Change to Localizables
+    var title: String {
+        return "Favorites"
+    }
+    
+    var removeFilterTitle: String {
+        return "Remove Filter"
+    }
+    
+    var numberOfFavorites: Int {
+        return self.movieViewModels.count
+    }
+    
+    private func acquireMovies(from favorites: [Int]) {
+        
+    }
+    
+    func getViewModel(for indexPath: IndexPath) -> MovieCellViewModel {
+        return self.movieViewModels[indexPath.row]
+    }
+    
+    func didTapFilter() {
+        
+    }
+    
+    func removeFilter() {
+        
+    }
+    
+    func selectMovie(at indexPath: IndexPath) {
+        
+    }
+    
+    func removeFavorite(at indexPath: IndexPath) {
+        let viewModel = getViewModel(for: indexPath)
+        
+        viewModel.didTapFavorite()
+    }
+    
+    
+}
