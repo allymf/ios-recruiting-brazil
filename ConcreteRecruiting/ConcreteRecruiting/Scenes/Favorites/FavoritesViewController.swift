413705c23c61afdcb38a1f530d5caa76b68b3b66
allymf <alyssonfacanhamoreira@gmail.com>
2020-01-12 23:03:04 -0300

    Add viewModel as attribute for viewController

--- ConcreteRecruiting/ConcreteRecruiting/Scenes/Favorites/FavoritesViewController.swift
+++ ConcreteRecruiting/ConcreteRecruiting/Scenes/Favorites/FavoritesViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
 
+    private let viewModel: FavoritesViewModel
+    
+    init(viewModel: FavoritesViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view = FavoritesView()
+        let favoritesView = FavoritesView()
+        self.view = favoritesView
+        favoritesView.setup(self.viewModel)
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "Filter"), style: .plain, target: self, action: #selector(didTapFilter))
         
